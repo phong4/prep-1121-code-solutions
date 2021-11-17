265e52254cc49704980dd09a5839470d62c3b208
Paul Hong <paulhong01@gmail.com>
2021-11-17 15:22:08 -0800

    Completed prep-javascript-functions excercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,59 @@
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+var sum = addTwoNumbers(2, 2);
+console.log('addTwoNumbers excercise', sum);
+
+function convertHourstoMinutes(hours) {
+  return 60 * 2;
+}
+sum = convertHourstoMinutes(60 * 2);
+console.log('convertHourstoMinutes excercise', sum);
+
+function getGreeting(name) {
+  alert('Hello World!');
+}
+getGreeting();
+
+function addAndMultiplyby5(num1, num2) {
+  num1 = 10;
+  num2 = 5;
+  return (num1 + num2) * 5;
+}
+sum = addAndMultiplyby5(10, 5);
+console.log('addAndMultiplyby5 excercise', sum);
+
+function multiplyAndDivideBy5(num1, num2) {
+  num1 = 35;
+  num2 = 10;
+  return (num1 * num2) / 5;
+}
+sum = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 excercise', sum);
+
+function subtractTwoNumbers(num1, num2) {
+  num1 = 22;
+  num2 = 7;
+  return (num1 - num2);
+}
+sum = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers excercise', sum);
+
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+console.log('getCircleCircumference excercise', getCircleCircumference(5));
+
+function getFullName(firstName, lastName) {
+  firstName = 'Juan';
+  lastName = 'Ramirez';
+  return firstName + ' ' + lastName;
+}
+sum = getFullName('Juan', 'Ramirez');
+console.log('getFullName excercise', sum);
+
+function cube(number) {
+  return 25 * 5;
+}
+sum = cube(5);
+console.log('cube excercise', sum);
