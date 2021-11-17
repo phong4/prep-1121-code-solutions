4318a520f3b97e8ae17bd0c6f7b098ca1bdd0f9a
Paul Hong <paulhong01@gmail.com>
2021-11-16 17:38:29 -0800

    Completed prep-javascript-arrays excercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+
+// eslint-disable-next-line no-unused-vars
+const firstElement = languages;
+console.log('the first element of the array is', languages[0]);
+
+const thirdElement = languages[2];
+console.log('the 3rd element of the array is', thirdElement);
+
+const length = languages.length;
+console.log('then length of the array is', length);
+
+const lastElement = languages[languages.length - 1];
+console.log('the last element of the array is', lastElement);
