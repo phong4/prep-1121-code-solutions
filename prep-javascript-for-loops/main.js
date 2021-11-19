e6c50a974ad685439d9367557fa09fbd2462eed1
Paul Hong <paulhong01@gmail.com>
2021-11-19 10:21:10 -0800

    Completed prep-javascript-for-loops excercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var x = 0; x < 20; x += 2) {
+  console.log(x);
+}
+
+for (var t = 100; t > 0; t--) {
+  console.log('Time till explosion ' + t + '!');
+}
